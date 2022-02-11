22fc451cc3604e8679d2ae9d786e4327f3511298
yan zhong <jackyan63@gmail.com>
2022-02-10 22:04:46 -0500

    singleton 2

---
+++ Chapter2/Item3/singleton/Elvis2.java
@@ -0,0 +1,8 @@
+package Chapter2.Item3.singleton;
+
+public class Elvis2 {
+    private static final Elvis2 INSTANCE = new Elvis2();
+    private Elvis2() {}
+    public static Elvis2 getInstance(){return INSTANCE;}
+    public void leaveTheBuilding() {}
+}
