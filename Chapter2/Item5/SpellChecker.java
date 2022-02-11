09330a44fa99ccf02af35c8e1e067bd73d0b9f87
yan zhong <jackyan63@gmail.com>
2022-02-11 18:49:05 -0500

    not understand

---
+++ Chapter2/Item5/SpellChecker.java
@@ -0,0 +1,21 @@
+package Chapter2.Item5;
+
+import java.util.List;
+import java.util.Objects;
+
+public class SpellChecker {
+    private final Lexicon dictionary;
+
+    public SpellChecker(Lexicon dictionary) {
+        this.dictionary = Objects.requireNonNull(dictionary);
+    }
+
+    public boolean isValid(String word) {
+        return false;
+
+    }
+
+    public List<String> suggestions(String typo) {
+        return null;
+    }
+}
